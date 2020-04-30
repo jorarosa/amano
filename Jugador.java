1ed7a26b3bbc861d7ff0b0867c44b267815f9fa4
José R. Rodríguez <jose.r.rodriguez.s@gmail.com>
2020-04-30 19:53:34 +0200

    Añadido Documentación.txt y modificado Jugador.java

--- Jugador.java
+++ Jugador.java
@@ -1,16 +1,18 @@
 import java.util.GregorianCalendar;
 
-public class Jugador {
+public abstract class Jugador {
 
 	private String apellido;
 	private GregorianCalendar fechaNacimiento;
+	private EnumPosicion posicion;
 	
 	public Jugador() {
 	}
 	
-	public Jugador(String apellido, GregorianCalendar fechaNacimiento) {
+	public Jugador(String apellido, GregorianCalendar fechaNacimiento, EnumPosicion posicion) {
 		this.setApellido(apellido);
 		this.setFechaNacimiento(fechaNacimiento);
+		this.setPosicion(posicion);
 	}
 	
 	public String getApellido() {
@@ -29,7 +31,17 @@ public class Jugador {
 		this.fechaNacimiento = fechaNacimiento; 
 	}
 
+	public abstract EnumPosicion getPosicion();
+		
+	public abstract void setPosicion(EnumPosicion posicion); 
+		
 	public String toString() {
 		return this.apellido + " - " + this.fechaNacimiento;
 	}
+}
+
+enum EnumPosicion {
+  PORTERO,
+  DEFENSA,
+  DELANTERO
 }	
